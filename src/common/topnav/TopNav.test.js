db40fc25b68e470d7271661ae71e620859b96f6b
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-10-08 23:34:17 +0200

    added tests to ensure correct props are passed to NavLinks

--- src/common/topnav/TopNav.test.js
+++ src/common/topnav/TopNav.test.js
@@ -11,10 +11,29 @@ function setup() {
 }
 
 describe("TopNav", function () {
+  it("should render a div with class of TopNav", function () {
+    const wrapper = setup()
+
+    expect(wrapper.is('div')).toBeTruthy()
+    expect(wrapper.hasClass('TopNav')).toBeTruthy()
+  });
   it("should render a NavLinks component", function () {
     const wrapper = setup()
 
     expect(wrapper.find('NavLinks').length).toBe(1)
+  });
+  it("should render the NavLinks component with props.classes of TopNav-NavLinks", function () {
+    const wrapper = setup()
+    const navLinks = wrapper.find('NavLinks')
+
+    expect(navLinks.prop("classes")).toEqual("TopNav-NavLinks")
+  });
+  it("should render the NavLinks Component with a list of links passed as props.links", function () {
+    const wrapper = setup()
+    const navLinks = wrapper.find('NavLinks')
+
+    expect(navLinks.prop("links")).toBeA(Array)
+    expect(navLinks.prop("links")[0]).toIncludeKeys(['path', 'name'])
   });
   it("should render a button with a class of close", function () {
     const wrapper = setup()

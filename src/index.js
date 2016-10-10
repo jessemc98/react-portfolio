369cdb1376f0040eafd732f84bb8e33bb04a102d
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-10-10 17:47:21 +0200

    removed obsolete material-ui code

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ import { Router, browserHistory } from 'react-router'
 import routes from './routes'
 import './style/index.scss'
 
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
-
 const rootElement = document.createElement('div')
 document.body.appendChild(rootElement)
 

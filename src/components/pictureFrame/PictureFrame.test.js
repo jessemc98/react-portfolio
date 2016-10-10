d3e369b5892988c61048c2033d8ce4f7b31ac898
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-10-10 20:57:58 +0200

    added PictureFrame Component skeleton

--- src/components/pictureFrame/PictureFrame.test.js
+++ src/components/pictureFrame/PictureFrame.test.js
@@ -1,7 +1,30 @@
 import React from 'react'
 import expect from 'expect'
-import enzyme from 'enzyme'
+import { shallow } from 'enzyme'
+
+import PictureFrame from './PictureFrame'
+
+function setup(props) {
+  return shallow(<PictureFrame {...props} />)
+}
 
 describe("PictureFrame", function () {
-  
+  it("should render a FabList", function () {
+    const wrapper = setup()
+    expect(wrapper.find('FabList').length).toEqual(1)
+  });
+  it("should pass 3 links to FabList", function () {
+    const wrapper = setup()
+    const fabList = wrapper.find('FabList')
+
+    expect(fabList.prop('links').length).toEqual(3)
+  });
+  it("first child should be an image", function () {
+    const wrapper = setup()
+
+    expect(wrapper.childAt(0).type()).toEqual('img')
+  });
+  it("should render with a class of PictureFrame", function () {
+
+  });
 });

427869ee39c70dc6a2579dd2328c410e8a5e3fc1
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-10-10 20:57:58 +0200

    added PictureFrame Component skeleton

--- src/components/pictureFrame/PictureFrame.js
+++ src/components/pictureFrame/PictureFrame.js
@@ -1,8 +1,16 @@
 import React, { PropTypes } from 'react'
+import FabList from '../../common/FabList/FabList'
+import './PictureFrame.scss'
+
+import { socialLinks } from '../../store/links'
+import picture from '../../../assets/images/logo.png'
 
 const PictureFrame = (props) => {
   return (
-    <div />
+    <div className="PictureFrame">
+      <img src={picture} />
+      <FabList links={socialLinks}/>
+    </div>
   )
 }
 

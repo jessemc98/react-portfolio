cee646aac86922f87c4c5941d483d22a5b40327d
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-10-12 20:52:59 +0200

    Info component for about page

---
+++ src/components/about/AboutInfo.js
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react'
+
+const AboutInfo = (props) => {
+  return (
+    <div className="AboutPage-AboutInfo">
+      <h1>Hi,</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco.
+      </p>
+    </div>
+  )
+}
+
+export default AboutInfo

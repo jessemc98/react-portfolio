cc4248aa4d4bfb8375c2aab6868dad215d659dce
Jesse <Jesse-Mac@Jesses-MBP.lan>
2016-10-24 05:54:28 +0200

    added form to contact page

--- src/components/contact/ContactPage.js
+++ src/components/contact/ContactPage.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import MyForm from '../myForm/Form'
+import MyInput from '../myForm/Input'
+import './ContactPage.scss'
 
 const ContactPage = (props) => {
   return (
-    <div>
-      ContactPage
+    <div id="ContactPage">
+      <h2>ContactPage</h2>
+      <MyForm>
+        <MyInput name="name" placeHolder="Name"/>
+        <MyInput name="email" type="email" placeHolder="Email"/>
+        <MyInput name="message" placeHolder="Message"/>
+      </MyForm>
     </div>
   )
 }

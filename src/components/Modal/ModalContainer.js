d13560ad4f623c6a9807c1a644d96a2daedb4949
Jesse McIntosh <jessemc98@hotmail.com>
2017-01-17 14:28:23 +0100

    pulled ReactCSSTransitionGroup out into the about page and changed ModalContainer to render a modal depending on browserHistory.location

--- src/components/Modal/ModalContainer.js
+++ src/components/Modal/ModalContainer.js
@@ -1,76 +1,29 @@
 import React, { PropTypes } from 'react'
-import ReactDOM from 'react-dom'
+import { browserHistory } from 'react-router'
 import Modal from './Modal'
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
-import './ModalContainer.scss'
+import modals from '../../store/modals'
 
 class ModalContainer extends React.Component {
-  constructor(props, context) {
-    super(props, context)
-    this.state = {
-      hidden: true
-    }
-    this.modalTarget = null
-
-    this.showModal = this.showModal.bind(this)
+  constructor(...args){
+    super(...args)
     this.hideModal = this.hideModal.bind(this)
-    this._render = this._render.bind(this)
-    this.cleanUp = this.cleanUp.bind(this)
   }
-  componentWillUpdate() {
-    this._render()
+  hideModal(){
+    browserHistory.push('/about')
   }
-  componentWillUnmount() {
-    this.hideModal()
-  }
-
-  _render() {
-    const { title, children, imgSrc } = this.props
-    ReactDOM.render(
-        <ReactCSSTransitionGroup
-          transitionName="modal"
-          transitionEnterTimeout={16}
-          transitionLeaveTimeout={0}>
-          {!this.state.hidden &&
-            <Modal key="modal"
-              title={title}
-              content={children}
-              imgSrc={imgSrc}
-              hide={this.hideModal}
-              unmount={this.cleanUp} />}
-        </ReactCSSTransitionGroup>,
-      this.modalTarget
-    )
-  }
-  cleanUp() {
-    setTimeout(() => {
-      ReactDOM.unmountComponentAtNode(this.modalTarget)
-      document.body.removeChild(this.modalTarget)
-    })
-  }
-  hideModal() {
-    this.setState({hidden: true}, () => this._render())
-  }
-  showModal() {
-    this.modalTarget = document.createElement('div')
-    this.modalTarget.className = "ModalContainer"
-    document.body.appendChild(this.modalTarget)
-    this.setState({hidden: false}, () => this._render())
-  }
-
-  render () {
-    const { imgSrc, title } = this.props
-    return (
-      <button className="ModalContainer_button" onClick={this.state.hidden?this.showModal:this.hideModal}>
-        <img src={imgSrc} alt={title + ' modal button'} />
-        <h3>{title}</h3>
-      </button>
-    )
+  render() {
+    const { modalId } = this.props.params
+      return (
+        <Modal {...modals[modalId]}
+          key="modal"
+          hide={this.hideModal} />
+      )
   }
 }
 ModalContainer.propTypes = {
-  children: PropTypes.element.isRequired,
-  title: PropTypes.string.isRequired,
-  imgSrc: PropTypes.string.isRequired
+  history: PropTypes.object.isRequired,
+  params: PropTypes.shape({
+    modalId: PropTypes.string.isRequired
+  }).isRequired
 }
 export default ModalContainer;

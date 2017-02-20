3d6de5cfaade90ac79d838fc46ea967b494367b8
Jesse McIntosh <jessemc98@hotmail.com>
2017-02-20 07:53:43 +0100

    dynamically added projects to projects page

--- src/components/projects/ProjectsPage.js
+++ src/components/projects/ProjectsPage.js
@@ -1,11 +1,19 @@
 import React, { PropTypes } from 'react'
 import Header from '../common/pageDivider/PageDivider'
+import Card from '../CardComponent/CardManager'
+import projects from './projects'
+
 import './ProjectPage.scss'
 
 const ProjectsPage = (props) => {
   return (
     <div className="ProjectsPage route">
       <Header title="Projects" />
+      <div className="ProjectsPage_cards">
+        {projects.map(project => (
+          <Card {...project} />
+        ))}
+        </div>
     </div>
   )
 }
